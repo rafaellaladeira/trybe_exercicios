fd92a7d1beb44b600535d145fd6cf6d270d54f43
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-11-26 18:18:34 -0300

    add exercicio de swich/case - aula

---
+++ 1-Fundamentos_de_desenvolvimento_web/bloco-04_Introducao_a_JavaScript/dia-1_javaScript_introducao_primeiros_passos/script3.js
@@ -0,0 +1,14 @@
+let estado = "vermelho";
+
+switch (estado) {
+    case "aprovada":
+        console.log("aprovada");
+        break;
+    case "lista":
+        console.log("lista");
+        break;
+    case "reprovada":
+        console.log("reprovada");
+    default:
+        console.log("Não se aplica");            
+}
