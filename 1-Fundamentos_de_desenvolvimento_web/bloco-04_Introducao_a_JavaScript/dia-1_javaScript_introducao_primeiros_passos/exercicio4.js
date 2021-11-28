b2ed8a9eab2ae91496e3b47f37e569ec7d13bb44
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-11-28 09:32:26 -0300

    add nome das variaveis no console.log

--- 1-Fundamentos_de_desenvolvimento_web/bloco-04_Introducao_a_JavaScript/dia-1_javaScript_introducao_primeiros_passos/exercicio4.js
+++ 1-Fundamentos_de_desenvolvimento_web/bloco-04_Introducao_a_JavaScript/dia-1_javaScript_introducao_primeiros_passos/exercicio4.js
@@ -1,35 +1,35 @@
 // 11)
 
-var salarioBruto = 2500;
+var salarioBruto = 4500;
 var salarioBase;
 var valorIR;
 
 // INSS
 
 if (salarioBruto <= 1556.94) {
-    console.log(salarioBase = salarioBruto * 0.92);
+    console.log("Salario base:" + (salarioBase = salarioBruto * 0.92));
 } else if ( salarioBruto <= 2594.92) {
-    console.log(salarioBase = salarioBruto * 0.91);
+    console.log( "Salario base:" + (salarioBase = salarioBruto * 0.91));
 } else if ( salarioBruto <= 5189.82) {
-    console.log(salarioBase = salarioBruto * 0.89);
+    console.log("Salario base:" + (salarioBase = salarioBruto * 0.89));
 } else if ( salarioBruto > 5189.82) {
-    console.log(salarioBase = salarioBruto - 570,88);
+    console.log("Salario base:" + (salarioBase = salarioBruto - 570,88));
 }
 
 // IR
 
 if (salarioBase <= 1903.98) {
-    console.log(valorIR = 0);
+    console.log("ValorIR:" + (0));
 } else if (salarioBase <= 2826.65) {
-    console.log (valorIR = (0.075 * salarioBase) - 142.80);
+    console.log ("Valor IR:" + (valorIR = (0.075 * salarioBase) - 142.80));
 } else if (salarioBase <= 3751.05) {
-    console.log(valorIR = (0.15 * salarioBase) - 354.80);
+    console.log("Valor IR:" + (valorIR = (0.15 * salarioBase) - 354.80));
 } else if (salarioBase <= 4664.68) {
-    console.log(valorIR = (0.225 * salarioBase) - 636.13);
+    console.log("Valor IR:" + (valorIR = (0.225 * salarioBase) - 636.13));
 } else if (salarioBase > 4664.68 ) {
-    console.log(valorIR = (0.275 * salarioBase) - 869.36);
+    console.log("Valor IR:" + (valorIR = (0.275 * salarioBase) - 869.36));
 }
 
 // Calculo do salário líquido :
 
-console.log("salarioLiquido:" + (salarioBase - valorIR));
+console.log("Salario Liquido:" + (salarioBase - valorIR));
