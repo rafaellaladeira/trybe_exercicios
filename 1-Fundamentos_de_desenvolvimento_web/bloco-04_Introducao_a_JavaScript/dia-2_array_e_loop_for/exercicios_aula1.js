20bba1c95a5c9f426915f8b156cd06cbfbe36bb7
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-11-29 14:59:37 -0300

    add os ultimos exercicios de for/of

--- 1-Fundamentos_de_desenvolvimento_web/bloco-04_Introducao_a_JavaScript/dia-2_array_e_loop_for/exercicios_aula1.js
+++ 1-Fundamentos_de_desenvolvimento_web/bloco-04_Introducao_a_JavaScript/dia-2_array_e_loop_for/exercicios_aula1.js
@@ -27,3 +27,17 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for (var i = 0 ; i < groceryList.length ; i+=1) {
 console.log(groceryList[i]);
 }
+
+// for/of :
+
+let arrOfNumbers = [10 , 20 , 30];
+for (let sum of arrOfNumbers) {
+    sum +=1;
+    console.log(sum);
+}
+
+// 7)
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let list of names){
+    console.log(list);
+}
