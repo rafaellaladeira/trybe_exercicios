3a912ce481568939316e39161b73244ac30805a4
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-12-01 18:35:47 -0300

    Exercicios das aulas

---
+++ 1-Fundamentos_de_desenvolvimento_web/bloco-04_Introducao_a_JavaScript/dia-4_objetos_e_funcoes/exercicios_aula.js
@@ -0,0 +1,56 @@
+// 1)
+
+let player = {
+    name: "Marta",
+    lastName: "Silva",
+    age: 34,
+    medal:{
+        golden: 2,
+        silver: 3,
+    }
+}
+
+// 2)  "A jogadora Marta Silva tem 34 anos de idade".
+
+let fullName = player.name + " " + player.lastName;
+
+console.log( "A jogadora " + fullName + " tem " + player.age + " anos de idade.")
+
+// 3) 
+
+player["bestInTheWorld"] = [2006, 2007, 2008, 2009, 2010, 2018];
+console.log(player);
+
+//4) "A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes".
+
+console.log(player.bestInTheWorld.length)
+
+console.log( "A jogadora " + fullName + " foi eleita a melhor do mundo por " + player.bestInTheWorld.length + " vezes.");
+
+//5) "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
+
+console.log("A jogadora possui " + player.medal.golden + " medalhas de ouro e " + player.medal.silver + " medalhas de prata.")
+
+//6) for/in: - Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
+
+  for (let name in names) {
+      console.log( "Olá, " + names[name]);
+  }
+
+//7)  Usando o objeto abaixo, utilize For/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (let cars in car) {
+      console.log(cars, car[cars]);
+  }
