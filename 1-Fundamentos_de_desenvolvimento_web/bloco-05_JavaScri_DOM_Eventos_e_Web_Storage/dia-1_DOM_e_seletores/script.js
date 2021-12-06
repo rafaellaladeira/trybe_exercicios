6f1535fddb043b2021d6d8e888c4ef7536fab97b
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-12-06 09:17:40 -0300

    Mais alguns exercicios

---
+++ 1-Fundamentos_de_desenvolvimento_web/bloco-05_JavaScri_DOM_Eventos_e_Web_Storage/dia-1_DOM_e_seletores/script.js
@@ -0,0 +1,10 @@
+document.querySelector('#header-container h1').style.background = 'rgb(76,164,109)'';
+document.getElementById('header-container').style.color = 'white';
+
+
+document.getElementsByTagName('section')[1].style.color = 'white';
+document.getElementsByClassName('no-emergency-tasks')[0].style.background = 'black';
+document.querySelectorAll('.emergency-tasks')[0].style.background = 'purple'
+
+document. getElementById('footer-container').style.background = 'black';
+document.getElementById('footer-container').style.color = 'white';
