0496aebc9e014fb9d4d7486c7b5fba7c921303e5
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-12-08 13:10:26 -0300

    exercicio do calendario

--- 1-Fundamentos_de_desenvolvimento_web/bloco-05_JavaScri_DOM_Eventos_e_Web_Storage/dia-3_eventos/main.js
+++ 1-Fundamentos_de_desenvolvimento_web/bloco-05_JavaScri_DOM_Eventos_e_Web_Storage/dia-3_eventos/main.js
@@ -9,19 +9,52 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // Pq o botão de "alterar a primeira tecnologia" 'empurrou' ela pra cima devido às suas alterações no CSS.
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
-
-
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+/* declarar a variavel para mexer 
+remover a class tech do li que tem essa classe
+linha do event.target para adicionar a classe
+
+*/ 
+
+function changeClass (event) {
+    const tech = document.querySelector('.tech');
+    tech.classList.remove('tech');
+    event.target.classList.add('tech');
+    input.value = '';
+}
+
+firstLi.addEventListener("click", changeClass);
+secondLi.addEventListener("click", changeClass);
+thirdLi.addEventListener("click", changeClass);
+
+ 
+  
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+
+input.addEventListener('input' , digit);
+
+function digit (event) {
+    let tech = document.querySelector('tech');
+    tech.innerText = event.target.value;
+}
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
 
+myWebpage.addEventListener('dblclick', function() {
+    window.location.replace('https://blog.betrybe.com/');
+})
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
+myWebpage.addEventListener('mouseover', function () {
+    const color = document.getElementById('.my-spotrybefy')[0];
+    color.style.color = 'pink'
+})
 
 // Segue abaixo um exemplo do uso de event.target:
 
