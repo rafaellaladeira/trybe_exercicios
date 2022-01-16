ab41bc147e9054b61f1d1fa8b09b522d02e9d612
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-01-16 16:49:51 -0300

    Exercicios bloco 2 e 3

---
+++ 1-Fundamentos_de_desenvolvimento_web/bloco-07_Introducao_a_JavaScript_ES6_e_Testes_Unitários/dia-2_Fluxo_de_exceção_e_objetos/script.js
@@ -0,0 +1,87 @@
+const sum = (value1, value2) => {
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+      return 'Os valores devem ser numéricos';
+    }
+    return value1 + value2;
+  };
+  
+  console.log(sum(2, '3'));
+
+  // ______________________________________________________________
+
+  const sum = (value1, value2) => {
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+      throw new Error('Os valores devem ser numéricos');
+    }
+    return value1 + value2;
+  };
+  
+  console.log(sum(2, '3'));
+
+  // _________________________________________________________________
+
+  const verifyIsNumber = (value1, value2) => {
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+      throw new Error('Os valores devem ser numéricos');
+    }
+  };
+  
+  const sum = (value1, value2) => {
+    try {
+      verifyIsNumber(value1, value2);
+      return value1 + value2;
+    } catch (error) {
+      throw error.message;
+    }
+  };
+  
+  console.log(sum(2, '3'));
+
+  // __________________________________________________________________
+
+  const customer = {
+    firstName: 'Roberto',
+    age: 22,
+    job: 'Teacher',
+  };
+  
+  let newKey = 'lastName';
+  const lastName = 'Ferreira';
+  customer[newKey] = lastName;
+  newKey = 'fullName';
+  const fullName = `${customer.firstName} ${customer.lastName}`;
+  customer[newKey] = fullName;
+  console.log(customer);
+
+  // __________________________________________________________________
+
+  const coolestTvShow = {
+    name: "BoJack Horseman",
+    genre: "adult animation",
+    author: "Raphael Bob-Waksberg",
+    favoriteCharacter: "Princess Carolyn",
+    quote: "Princess Carolyn always lands on her feet.",
+    seasons: 6,
+  };
+  
+  for (const property in coolestTvShow) {
+    console.log(property);
+  }
+  
+  // __________________________________________________________________
+
+
+const person = {
+    name:'Roberto',
+  };
+  
+  const lastName = {
+    lastName: 'Silva',
+  };
+  
+  const newPerson = Object.assign({},person,lastName);
+  newPerson.name = 'Gilberto';
+  console.log(newPerson);
+  console.log(person);
+
+  // ___________________________________________________________________
