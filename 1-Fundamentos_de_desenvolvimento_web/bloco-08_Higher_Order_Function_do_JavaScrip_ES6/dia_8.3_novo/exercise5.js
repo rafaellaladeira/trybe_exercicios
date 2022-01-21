1c0e56a4002a38a98f129b6e221e8718f33e7fce
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-01-20 22:17:24 -0300

    Exercicios do dia

---
+++ 1-Fundamentos_de_desenvolvimento_web/bloco-08_Higher_Order_Function_do_JavaScrip_ES6/dia_8.3_novo/exercise5.js
@@ -0,0 +1,12 @@
+// 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+
+const expectedResult = [
+    'Frank Herbert',
+    'George R. R. Martin',
+    'Isaac Asimov',
+    'J. R. R. Tolkien',
+    ];
+    function fantasyOrScienceFictionAuthors() {
+    // escreva seu código aqui
+    }
+    
---
+++ 1-Fundamentos_de_desenvolvimento_web/bloco-08_Higher_Order_Function_do_JavaScrip_ES6/dia_8.3_novo/exercise6.js
@@ -0,0 +1,12 @@
+
+// 6 - Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
+
+const expectedResult = [
+    'O Senhor dos Anéis',
+    'Fundação',
+    'O Chamado de Cthulhu',
+    ];
+    function oldBooks() {
+    // escreva seu código aqui
+    }
+    
