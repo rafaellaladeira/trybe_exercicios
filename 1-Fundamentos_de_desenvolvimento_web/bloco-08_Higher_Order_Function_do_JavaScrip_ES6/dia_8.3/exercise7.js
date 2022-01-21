fac4149b691d8fc5c4f3b6911895b7fe4153bb27
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-01-20 21:58:48 -0300

    Exericios do dia

---
+++ 1-Fundamentos_de_desenvolvimento_web/bloco-08_Higher_Order_Function_do_JavaScrip_ES6/dia_8.3/exercise7.js
@@ -0,0 +1,7 @@
+// 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais. Dica: cada inicial termina com um ponto.
+
+const expectedResult = 'O Senhor dos Anéis';
+
+function authorWith3DotsOnName() {
+  // escreva seu código aqui
+}
