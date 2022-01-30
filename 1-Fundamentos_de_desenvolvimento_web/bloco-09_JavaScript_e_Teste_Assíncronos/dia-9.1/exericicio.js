0c2d176e178cfa3c45f68deb44d72e9afcc52581
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-01-30 19:17:50 -0300

    Exercicios do dia - do course

---
+++ 1-Fundamentos_de_desenvolvimento_web/bloco-09_JavaScript_e_Teste_Assíncronos/dia-9.1/exericicio.js
@@ -0,0 +1,155 @@
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+setTimeout(() => console.log(numbers), 3000);
+
+// _______________________________________________
+
+const despesas = [
+    {
+      gym: 99,
+    },
+    {
+      ifood: 200,
+    },
+    {
+      phone: 60,
+    },
+    {
+      internet: 100,
+    },
+  ];
+  
+  const renda = 1000;
+
+  const despesaMensal = (renda, despesas, callback) => {
+
+    const despesaTotal = callback(despesas);
+    const saldoFinal = renda - despesaTotal;
+  
+    console.log(`Balanço do mês:
+      Recebido: R$${renda},00
+      Gasto: R$${despesaTotal},00
+      Saldo: R$${saldoFinal},00 `);
+  };
+  const somaDespesas = (despesas) => {
+    const custoItem = despesas.map((item) => Object.values(item));
+    const despesaTotal = custoItem.reduce((acc, curr) => acc += curr[0], 0);
+    return despesaTotal;
+  };
+  
+  // acc é a sigla para accumulator (acumulador)
+  // curr é a sigla para current (valor atual)
+  
+  despesaMensal(renda, despesas, somaDespesas);
+
+  // __________________________________________________
+
+  // Definição da função userFullName
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+
+// Definição da função getUser
+const getUser = (param) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  // Retornamos nosso parâmetro, que será uma função (callback)
+  return param(userToReturn);
+};
+
+// Chamada/execução da função getUser, que vai receber como parâmetro nossa função userFullName.
+getUser(userFullName);
+
+// ______________________________________________________
+
+// Definição da função userFullName
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality}) => `${firstName} is ${nationality}`;
+
+// Definição da função getUser
+const getUser = (param) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  // Retornamos nosso parâmetro, que será uma função (callback)
+  return param(userToReturn);
+};
+
+// Chamada/execução da função getUser, que vai receber como parâmetro nossa função userFullName.
+console.log(getUser(userFullName));
+console.log(getUser(userNationality));
+
+// _________________________________________________________--
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    // Retorne a `callback` passada como parâmetro na função `getUser`
+    // Dica: você pode manter o `console.log()`
+    console.log(callback(user));
+  }, delay());
+};
+
+
+
+getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+
+// _______________________________________________________________________
+
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, callback) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    } else {
+      const errorMessage = "Country could not be found";
+      callback(errorMessage);
+    }
+  }, delay());
+};
+
+// Deve imprimir "Returned country is Brazil" no sucesso ou "Error getting country: Country could not be found" em caso de falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em caso de falha
+getCountry(countryCurrency, printErrorMessage);
+
+// _____________________________________________________________________
+
+test('Não deveria passar!', () => {
+    setTimeout(() => {
+      expect(10).toBe(5);
+      console.log('Deveria falhar!');
+    }, 500);
+  });
