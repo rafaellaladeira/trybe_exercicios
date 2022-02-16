dc61a15de93b6c2460e52d31266936e946fec601
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-02-16 18:20:20 -0300

    Exercicios de React

---
+++ 2-Front_end/bloco_10_Introdução_à_React/dia_1_ Apresentação_do_React/Exercicios_do_conteúdo/pasta/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './script';
+import Exercise from './Component';
+
+function App() {
+  return (<Exercise/>)
+}
+
+export default App;
---
+++ 2-Front_end/bloco_10_Introdução_à_React/dia_1_ Apresentação_do_React/Exercicios_do_conteúdo/pasta/src/Component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Exercise extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Rafaella Ladeira</h1>
+                <p>Eu sou eu!</p>
+            </div>
+        )
+    }
+}
+export default Exercise;
