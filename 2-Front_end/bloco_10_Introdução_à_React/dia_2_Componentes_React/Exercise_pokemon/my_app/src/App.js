17a2000fda19e7a8dd669d9d8394c860cf317ac6
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-02-18 10:19:19 -0300

    Execise pokemon

---
+++ 2-Front_end/bloco_10_Introdução_à_React/dia_2_Componentes_React/Exercise_pokemon/my_app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import pokemons from './data';
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex pokemons={pokemons} />
+      </div>
+  );
+}
+}
+export default App;
