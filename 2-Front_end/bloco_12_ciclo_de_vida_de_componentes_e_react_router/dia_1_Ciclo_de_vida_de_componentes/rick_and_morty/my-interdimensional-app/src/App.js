50fe0d2737d8f9f9d0f97dbceebb745589aafc3c
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-03-03 19:18:16 -0300

    Exercicio do Rick and Morty

---
+++ 2-Front_end/bloco_12_ciclo_de_vida_de_componentes_e_react_router/dia_1_Ciclo_de_vida_de_componentes/rick_and_morty/my-interdimensional-app/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component }from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    console.log('constructor');
+    super();
+    this.state = {
+        characters: [],
+    };
+  }
+
+  componentDidMount () {
+    console.log('component');
+    const result = await fetch('https://rickandmortyapi.com/api/character')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({characters: data.results})
+    })
+   }  
+
+
+   render() {
+     console.log('render');
+    const { characters } = this.state;
+    return (
+      <div className="App">
+        <h1>
+          Ricky and Morty Characters:
+        </h1>
+        <div className="body">
+          {characters.map((character) => {
+            return (
+              <div className="container" key={character.name}>
+                <h3>{character.name}</h3>
+                <img src={character.image} alt={character.name}/>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
